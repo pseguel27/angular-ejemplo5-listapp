6fe0c0e980f3bdb965b687a10e9ef52ff935df29
pseguel27 <pato.seguel.moya@gmail.com>
2018-03-06 00:09:10 -0300

    se inicia nuevo proyecto ionic, lista app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
+import { PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
+import { AgregarComponent } from '../pages/agregar/agregar.component';
+
+import { ListaDeseosService } from './services/lista-deseos.service';
+
+//pipes
+import { PlaceholderPipe } from './pipes/placeholder.pipe';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +20,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent,
+    AgregarComponent,
+    PlaceholderPipe
   ],
   imports: [
     BrowserModule,
@@ -26,14 +33,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent,
+    AgregarComponent
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ListaDeseosService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
+import { NavController } from 'ionic-angular';
+import { AgregarComponent } from '../agregar/agregar.component';
+
+
+
+@Component({
+  selector: 'app-pendientes',
+  templateUrl: 'pendientes.component.html'
+})
+export class PendientesComponent implements OnInit {
+
+  	constructor( private _listaDeseos: ListaDeseosService, private NavCtrl:NavController ) { }
+
+  	ngOnInit(){
+  	
+  	}
+
+
+	irAgregar(){
+		this.NavCtrl.push( AgregarComponent )
+	}
+
+}
